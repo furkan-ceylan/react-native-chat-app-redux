7f88b5b0ce3857d8e5a5cd8dec4ecd3ccf32bc73
fcyln <ceylan.furkan@outlook.com>
2018-05-26 02:08:31 +0300

    ikinci commit

---
+++ index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry
+} from 'react-native';
+
+import FCM, { FCMEvent, NotificationType } from 'react-native-fcm';
+
+import App from './src/App.js';
+
+export default class ChatApp extends Component {
+    render() {
+        return (
+            <App />
+        );
+    }
+}
+
+AppRegistry.registerComponent('tolkin', () => ChatApp);
